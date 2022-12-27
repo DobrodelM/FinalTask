9b922b559e594e526ab1b540cb1c7346f72e2164
DobrodelM <118010165+DobrodelM@users.noreply.github.com>
2022-12-28 02:12:54 +0300

    Add files via upload

---
+++ Code/Program.cs
@@ -0,0 +1,54 @@
+﻿int getUserInt(string message)
+{
+    Console.Write(message);
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+string[] GenerateUserArray(int length)
+{
+    string[] newArray = new string[length];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+void printArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("[]");
+    }
+    else
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine(array[array.Length - 1] + "]");
+    }
+}
+string[] FormNewArray(string[] array, int number)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= number)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
+int size = getUserInt("Введите длину массива: ");
+string[] array = GenerateUserArray(size);
+printArray(array);
+int maxWordLength = getUserInt("Введите максимальную длину строки в символах: ");
+string[] newArray = FormNewArray(array, maxWordLength);
+printArray(newArray);
